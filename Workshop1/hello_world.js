e0de490ba2747d6c5da8c21a9de0e0ebdc7b4d73
Kimberly Lewis <kblewis.flt@gmail.com>
2016-07-22 15:05:43 -0500

    hello world strobe created

---
+++ Workshop1/hello_world.js
@@ -0,0 +1,15 @@
+//Set johnny-five dependency
+var five = require("johnny-five");
+//Set up references to the board and the led
+var board, led;
+
+//initialize the microcontroller
+board = new five.Board();
+
+//if the initialization is successful, run this block of code
+board.on("ready", function() {
+   //set up an led object with the pin at 13
+    led = new five.Led(13);
+    //strobe the led
+    led.strobe(300);
+});
