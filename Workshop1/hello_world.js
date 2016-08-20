8b3da1f5b74a9b87344bfdc4daf094acfdd710f1
Kimberly Lewis <kblewis.flt@gmail.com>
2016-08-20 12:26:41 -0500

    what

--- Workshop1/hello_world.js
+++ Workshop1/hello_world.js
@@ -8,7 +8,7 @@ board = new five.Board();
 
 //if the initialization is successful, run this block of code
 board.on("ready", function() {
-   //set up an led object with the pin at 13
+    //set up an led object with the pin at 13
     led = new five.Led(13);
     //strobe the led
     led.strobe(300);
